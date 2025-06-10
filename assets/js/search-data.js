2c8e6db7b9995893a68b2132367eae72f25e5a75
higd963 <48345342+higd963@users.noreply.github.com>
2025-06-10 06:05:59 +0000

    Deploying to gh-pages from @ higd963/higd963.github.io@84cae225c70924421e7084f7513bdff81d40ea04 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,12 +419,7 @@ ninja.data = [{
           section: "News",},{id: "news-a-paper-has-been-published-at-ieee-transactions-on-network-science-and-engineering-jcr-top-3-3",
           title: 'A paper has been published at IEEE Transactions on Network Science and Engineering...',
           description: "",
-          section: "News",},{id: "projects-ecmwf-annual-seminar-2025-apr-2025",
-          title: 'ECMWF Annual Seminar 2025 (Apr. 2025)',
-          description: "",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/Create%20academic_2025_ecmwf/";
-            },},{id: "projects-cikm-conference-at-beijing-nov-2019",
+          section: "News",},{id: "projects-cikm-conference-at-beijing-nov-2019",
           title: 'CIKM Conference at Beijing (Nov. 2019)',
           description: "",
           section: "Projects",handler: () => {
@@ -479,6 +474,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/academic_2024_aaai/";
+            },},{id: "projects-ecmwf-annual-seminar-2025-apr-2025",
+          title: 'ECMWF Annual Seminar 2025 (Apr. 2025)',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/academic_2025_ecmwf/";
             },},{id: "projects-ai-seminar-at-cuk-mar-2025",
           title: 'AI Seminar at CUK (Mar. 2025)',
           description: "",
